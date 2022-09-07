6633e389b453eebe8b5fd7c3ac77e9dc6c076653
ibatista04 <111529763+ibatista04@users.noreply.github.com>
2022-09-07 17:26:41 -0500

    Comienzo
    Comienzo

---
+++ Clase 5 deg.R
@@ -0,0 +1,10 @@
+#Iván Arturo
+#7 de septiembre del 2022
+## instalar/llamar pacman SIEMPRE HACERLO
+require(pacman)
+
+## usar la función p_load de pacman para instalar/llamar las librerías de la clase
+p_load(rio, # función import/export: permite leer/escribir archivos desde diferentes formatos. 
+       skimr, # funcion skim: describe un conjunto de datos
+       janitor, # función tabyl: frecuencias relativas
+       dplyr) # contiene conjuntos de datos.
