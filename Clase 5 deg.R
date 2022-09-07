108c24d6c7241c3ec6b52d1f194ad484b944f351
ibatista04 <111529763+ibatista04@users.noreply.github.com>
2022-09-07 17:39:51 -0500

    Cambio 1
    Cambio 1

--- Clase 5 deg.R
+++ Clase 5 deg.R
@@ -1,6 +1,8 @@
 #Iván Arturo
 #7 de septiembre del 2022
 ## instalar/llamar pacman SIEMPRE HACERLO
+rm(list-ls())
+
 require(pacman)
 
 ## usar la función p_load de pacman para instalar/llamar las librerías de la clase
@@ -8,3 +10,8 @@ p_load(rio, # función import/export: permite leer/escribir archivos desde difer
        skimr, # funcion skim: describe un conjunto de datos
        janitor, # función tabyl: frecuencias relativas
        dplyr) # contiene conjuntos de datos.
+
+##Importar datos
+data_csv = import(file="input/datos_censo.csv" , skip=6 , encoding="UTF-8")
+
+data_xls = import(file="input/datos_excel.xlsx" , skip=9)
